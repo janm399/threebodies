4cfa018cfb739e60866ec8f2ea1adc7a381ceca1
Jan Machacek <jan.machacek@gmail.com>
2020-12-07 13:27:10 +0000

    Latest'

--- source/2020-12-xx-httpclientperf/cpp/main.cpp
+++ source/2020-12-xx-httpclientperf/cpp/main.cpp
@@ -1,9 +1,12 @@
 #include <boost/asio/strand.hpp>
+#include <ctime>
 #include <boost/beast.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/ssl.hpp>
 #include <boost/beast/version.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <cstdlib>
 #include <fstream>
 #include <functional>
@@ -17,82 +20,102 @@ namespace http = beast::http;      // from <boost/beast/http.hpp>
 namespace net = boost::asio;       // from <boost/asio.hpp>
 namespace ssl = boost::asio::ssl;  // from <boost/asio/ssl.hpp>
 using tcp = boost::asio::ip::tcp;  // from <boost/asio/ip/tcp.hpp>
+using mc = boost::posix_time::microsec_clock;
 
 using namespace std;  // so shoot me
-
-vector<string> getUris() {
-  ifstream uri_file("../uris.txt");
-  vector<string> result;
-  copy(istream_iterator<string>(uri_file), istream_iterator<string>(),
-       back_inserter(result));
-  return result;
-}
 
 struct request_benchmark {
   string uri;
   long elapsed_time;
   long response_size;
+
+  friend ostream &operator<<(ostream &os, const request_benchmark &o) {
+    os << o.uri << "," << o.elapsed_time << "," << o.response_size << endl;
+    return os;
+  }
 };
 
 struct run_requests {
   vector<request_benchmark> responses;
   long elapsed_time;
 
-  friend ostream& operator<<(ostream& os, const run_requests& p) {
-    // sort(begin(p.responses), end(p.responses),
-    //      [](auto const& a, auto const& b) { return a.uri < b.uri; });
-
+  friend ostream &operator<<(ostream &os, const run_requests &o) {
+    auto responses = o.responses;
+    sort(begin(responses), end(responses),
+          [](auto const& a, auto const& b) { return a.uri < b.uri; });
+    for (const auto &response : responses) os << response << endl;
+    os << o.elapsed_time << endl;
     return os;
   }
 };
 
-int main() {
-  auto const host = "github.com";
-  auto const port = "443";
-  auto const target = "/robots.txt";
-  auto const version = 11;  // HTTP version 1.1
-
+class benchmark {
+private:
   net::io_context ioc;
-  // The SSL context is required, and holds certificates
-  ssl::context ctx{ssl::context::tlsv12_client};
+  ssl::context ctx;
+  tcp::resolver resolver;
+  vector<string> uris;
+
+public:
+  benchmark();
+
+  request_benchmark one_run(const string &host, const string &port,
+                            const string &target);
+};
+
+benchmark::benchmark(): ctx{ssl::context::tlsv12_client}, resolver(ioc) {
   ctx.load_verify_file("/etc/ssl/cert.pem");
   ctx.set_verify_mode(ssl::verify_peer);
-  tcp::resolver resolver(ioc);
+  ifstream uri_file("../uris.txt");
+  copy(istream_iterator<string>(uri_file), istream_iterator<string>(),
+       back_inserter(uris));
+}
+
+request_benchmark benchmark::one_run(const string &host, const string &port,
+                                     const string &target) {
+  constexpr int version = 11;
+
+  const auto start = mc::local_time();
+
   beast::ssl_stream<beast::tcp_stream> stream(ioc, ctx);
-  if (!SSL_set_tlsext_host_name(stream.native_handle(), host)) {
+  if (!SSL_set_tlsext_host_name(stream.native_handle(), host.c_str())) {
     beast::error_code ec{static_cast<int>(::ERR_get_error()),
                          net::error::get_ssl_category()};
     throw beast::system_error{ec};
   }
+
   auto const results = resolver.resolve(host, port);
   beast::get_lowest_layer(stream).connect(results);
   stream.handshake(ssl::stream_base::client);
   http::request<http::string_body> req{http::verb::get, target, version};
   req.set(http::field::host, host);
   req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-
-  // Send the HTTP request to the remote host
-  http::write(stream, req);
-
-  // This buffer is used for reading and must be persisted
   beast::flat_buffer buffer;
-
-  // Declare a container to hold the response
   http::response<http::dynamic_body> res;
-
-  // Receive the HTTP response
   http::read(stream, buffer, res);
 
-  // Write the message to standard out
-  cout << res << endl;
-
-  // Gracefully close the stream
   beast::error_code ec;
   stream.shutdown(ec);
-  if (ec == net::error::eof) {
+  if (ec == net::error::eof)
     ec = {};
-  }
-  if (ec) throw beast::system_error{ec};
+  if (ec)
+    throw beast::system_error{ec};
+  const auto end = mc::local_time();
+
+  return {
+      .uri = host,
+      .elapsed_time = (end - start).total_milliseconds(),
+      .response_size = 0L,
+  };
+}
+
+int main() {
+  auto const host = "github.com";
+  auto const port = "443";
+  auto const target = "/robots.txt";
+
+  benchmark m;
+  cout << m.one_run(host, port, target) << endl;
 
   return 0;
 }
---
+++ source/2020-12-xx-httpclientperf/cpp/session.h
@@ -0,0 +1,14 @@
+#ifndef HTTPCLIENTPERF_SESSION_H
+#define HTTPCLIENTPERF_SESSION_H
+
+class session {};
+
+class synchronous_session : session {
+
+};
+
+class asynchronous_session : session {
+
+};
+
+#endif
